bc4fafc05523ded51bc16cd20ae60ca92a5a6f5f
AllopisQW124 <93017110+AllopisQW124@users.noreply.github.com>
2023-02-21 07:31:17 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+  Vless_UUID="f0cd3ad2-e2ba-440a-898e-028e8f4627e5"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+  Vmess_UUID="f0cd3ad2-e2ba-440a-898e-028e8f4627e5"
 fi
 echo ${Vmess_UUID}
 
