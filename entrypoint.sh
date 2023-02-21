95803e1293562d381f21e3b9bec8c634baa6e3e8
AllopisQW124 <93017110+AllopisQW124@users.noreply.github.com>
2023-02-21 07:42:39 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/vl555"
+  Vless_Path="/vless"
 fi
 echo ${Vless_Path}
 
@@ -17,7 +17,7 @@ fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="vm666"
+  Vmess_Path="vmess"
 fi
 echo ${Vmess_Path}
 
